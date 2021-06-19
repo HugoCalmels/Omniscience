79b30a56ae616599573ecff9bfffd7a1cee41100
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-06-19 19:34:13 +0200

    heroku has stopped working ..

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2021_06_19_165212) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
