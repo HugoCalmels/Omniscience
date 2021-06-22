ce6810bbfa0b1e96440f7ddf2d2450c55617fda9
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-06-22 15:12:54 +0200

    trying with sendgrid & heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,14 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey', 
+  password: Rails.credientals.dig(:sendgrid, :api_key), 
+  domain: 'omniscience-thp.herokuapp.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 
 
