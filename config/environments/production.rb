c027b94ffcddab56eb2fb358db9ac297a62f4b57
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-06-22 22:35:17 +0200

    changed to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,19 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-    port: 587,
-    address: 'smtp.mailgun.org',
-    domain: ENV['DOMAIN_NAME'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: :plain
-  }
-
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-  api_key: ENV['MAILGUN_API_KEY'],
-  domain:ENV['DOMAIN_NAME'],
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  port: 587,
+  address: 'smtp.gmail.com',
+  user_name: ENV['SMTP_USER_NAME'],
+  password: ENV['SMTP_PASSWORD'],
+  authentication: :plain,
+  enable_starttls_auto: true
+}
   # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
   }
 
