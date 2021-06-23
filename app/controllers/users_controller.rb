ea94d8731db7dead6dfcddd5ff22be91691c61f1
aurelflip <aur.ama972@gmail.com>
2021-06-23 19:16:22 +0200

    profil chaged new cards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @user_videos = @user.videos
+    #@user_videos = @user.videos
+    @user_videos = Video.where(user_id: params[:id]) 
   end
 
   def index
   end
+  
 end
