57e07826cfbac6459f3a758fbcbbc4ec11c08f06
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-06-25 17:26:08 +0200

    shouldnt have tried smth right now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,23 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-<<<<<<< HEAD
   config.action_mailer.perform_caching = false
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
-    config.action_mailer.asset_host = ENV['DOMAIN_NAME']
-
-    config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :authentication       => :plain,
-      :user_name            => ENV['SMTP_USER_NAME'],
-      :password             => ENV['SMTP_PASSWORD'],
-      :enable_starttls_auto => true
-    }
-=======
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'omniscience-thp.herokuapp.com' }
->>>>>>> release/1.0.7
+  ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 465,
+    domain: ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_USER_NAME'],
+    authentication: 'plain',
+    :ssl => true,
+    :tsl => true,
+    enable_starttls_auto: true 
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
